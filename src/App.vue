93ac08c09866f35fb1215d3b81e288721688711a
rasujon3@gmail.com <rasujon3@gmail.com>
2024-02-16 20:53:44 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1>{{ courseName }}</h1>
+  <p>Course by {{ mentor }}</p>
+
+  <button @click="count++">+</button>
+    <h3>{{ count }}</h3>
+  <button @click="count--">-</button>
+</template>
+
+// options API
+<!-- <script>
+export default{
+  data(){
+    return {
+      courseName: "Vue js 3 masterclass",
+      mentor: "Sujon",
+      count: 0,
+    }
+  }
+}
+</script> -->
+// Composition API
+<!-- <script>
+export default{
+  setup() {
+    let count = 0;
+    const courseName = "Vue 3 Masterclass";
+    const mentor = "Sujon";
+
+    return {count, courseName, mentor}
+  }
+}
+</script> -->
+
+<script setup>
+import { ref } from 'vue';
+    let count = ref(0);
+    const courseName = ref("Vue 3 Masterclass");
+    const mentor = ref("Sujon");
+</script>
